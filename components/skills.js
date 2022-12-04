feb10f6395bc13bdc415018eefe845bf5cc055a4
unknown <oguzhandemiraslan@gmail.com>
2022-12-04 16:11:55 -0500

    header updated

---
+++ components/skills.js
@@ -0,0 +1,12 @@
+function Skills() {
+  return (
+    <div>
+      <h2>Skills</h2>
+      <div>
+        <img src="../img/html.png"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { Fragment } from "react";
 import Header from "../components/header";
+import Skills from "../components/skills";
 
 function HomePage() {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <Skills />
+    </Fragment>
+  );
 }
 
 export default HomePage;
