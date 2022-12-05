8c8805737c5b7c827af2507590b821fb187b6d29
unknown <oguzhandemiraslan@gmail.com>
2022-12-04 21:52:33 -0500

    skills section added

--- components/skills.js
+++ components/skills.js
@@ -1,9 +1,45 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import classes from "./skills.module.css";
+
 function Skills() {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
   return (
-    <div>
-      <h2>Skills</h2>
-      <div>
-        <img src="../img/html.png"></img>
+    <div className={classes.container}>
+      <h2 className={classes.title}>Skills</h2>
+      <div className={classes.skills}>
+        <img
+          src="images/html.png"
+          data-aos="fade-up"
+          className={classes.logo}
+        />
+        <img src="images/css.png" data-aos="fade-up" className={classes.logo} />
+        <img src="images/js.png" data-aos="fade-up" className={classes.logo} />
+        <img
+          src="images/react.png"
+          data-aos="fade-up"
+          className={classes.logo}
+        />
+        <img
+          src="images/nextjs.png"
+          data-aos="fade-up"
+          className={classes.logo}
+        />
+        <img
+          src="images/node.png"
+          data-aos="fade-up"
+          className={classes.logo}
+        />
+        <img
+          src="images/mongo.png"
+          data-aos="fade-up"
+          className={classes.logo}
+        />
+        <img src="images/git.png" data-aos="fade-up" className={classes.logo} />
       </div>
     </div>
   );
