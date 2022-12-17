59e8d6d38d066aad893daae90724d6dc56b0d578
unknown <oguzhandemiraslan@gmail.com>
2022-12-17 16:18:58 -0500

    meta tag updated

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,10 @@ class MyDocument extends Document {
             name="description"
             content="You can find more information about me, my projects and more..."
           />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
         </Head>
         <body>
           <Main />
